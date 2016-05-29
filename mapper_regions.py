ce19ab3878f1bb0a63decbcdfda5b734ed348803
rodrigo <rodfcast@espol.edu.ec>
2016-05-28 19:50:32 -0500

    mapper y reducer para agregar por celdas geograficas

---
+++ mapper_regions.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python2
+import sys
+import datetime
+
+def getCellRegion(cellId):
+    if cellId <= 3300:
+        tmp = str(cellId)
+        if len(tmp) == 1:
+            return 7 #suroeste
+        else:
+            cellComp = int(tmp)
+            if len(tmp) > 2:
+                tmp2 = tmp[-2:]
+                cellComp = int(tmp2)
+            if cellComp < 33:
+                return 7 #suroeste
+            elif cellComp > 66:
+                return 9 #sureste
+            else:
+                return 8 #sur
+    elif 3301 <= cellId <= 6600:
+        cellComp = int((str(cellId))[-2:])
+        if 0 < cellComp < 33:
+            return 4  #oeste
+        elif cellComp > 66:
+            return 6  #este
+        else:
+            return 5  #centro
+    else:
+        cellComp = int((str(cellId))[-2:])
+        if 0 < cellComp < 33 :
+            return 1 #noroeste
+        elif cellComp > 66 or cellComp == 0:
+            return 3 #noreste
+        else:
+            return 2 #norte
+
+for line in sys.stdin:
+	line = line.strip()
+	words = line.split('\t')
+
+	id = int(words[0])
+	time_interval =int(words[1])
+	code = int(words[2])
+
+	try:
+		call_out = words[6]
+		if call_out != '':
+			call_out =  float(words[6])
+			region_id = getCellRegion(id)
+			print '%d\t%s\t%s\t%.4f' % (region_id,code,time_interval,call_out)
+	except IndexError:
+		continue
---
+++ reducer_regions.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python2
+from operator import itemgetter
+import sys
+import datetime
+#bandas horarias de una hora
+
+def get_hour_band(time_interval):
+	date =  datetime.datetime.fromtimestamp(int(time_interval)/1000)
+	return date.hour
+
+saved_id_region = None
+saved_band_hour = None
+saved_time_interval = None
+saved_code = None
+count_call_out = 0
+
+for line in sys.stdin:
+	line = line.strip()
+	region_id,code,time_interval,call_out = line.split('\t')
+	try: 
+		region_id = int(region_id)
+		call_out = float(call_out)
+	except ValueError:
+		continue 
+
+	band_hour = get_hour_band(time_interval)
+
+	if  saved_band_hour == band_hour : # si la banda horaria es igual a la banda horaria que hemos registrado antes(current_band_hour), sumamos la actividad de llamadas
+		count_call_out += call_out
+	else:
+		if saved_band_hour:
+			print '%d\t%s\t%d\t%.4f' % (saved_id_region, saved_code, saved_band_hour, count_call_out)
+			#date = datetime.datetime.fromtimestamp(int(saved_time_interval)/1000).strftime('%Y-%m-%d/%H:%M:%S')
+			#print '%d\t%s\t%s\t%s\t%d\t%.4f' % (saved_id_region, saved_code, saved_time_interval, date, saved_band_hour, count_call_out)
+		saved_id_region = region_id
+		saved_code = code
+		saved_time_interval = time_interval
+		saved_band_hour = band_hour
+		count_call_out = call_out
+		
+#ultima iteracion
+if  saved_band_hour == band_hour :
+	print '%d\t%s\t%d\t%.4f' % (saved_id_region, saved_code, saved_band_hour, count_call_out)
+	#print '%d\t%s\t%s\t%d\t%.4f' % (saved_id_region,saved_code, date, saved_band_hour, count_call_out)
